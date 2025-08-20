aa4d053d0475c4611a6ce9a804ba66ca8bbcd2ee
Vrindaarora2008 <vrindaaroraa83@gmail.com>
2025-08-20 11:10:47 +0530

    Create ROOTS.c

---
+++ ROOTS.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c,D;
+printf("Enter value of a from the equation.\n");
+scanf("%d", &a);
+printf("Enter value of b from the equation.\n");
+scanf("%d", &b);
+printf("Enter value of c from the equation.\n");
+scanf("%d", &c);
+D=(b*b)-4*a*c;
+if(D>0)
+{
+    printf("THE ROOTS ARE REAL AND DISTINCT");
+}
+else if(D=0)
+{
+    printf("THE ROOTS ARE REAL AND EQUAL");
+}
+else
+{
+    printf("THE ROOTS ARE IMAGINARY");
+}
+return 0;
+}
